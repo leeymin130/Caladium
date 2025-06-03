fa409e714b783689261775f1d86b1bc99c4e6fdc
yoomin <leeymin130@gmail.com>
2025-06-03 21:34:22 +0900

    Project setting
    1. 레포지토리 생성
    2. .gitignore 파일 추가
    3. develop 브랜치 추가 (default 설정)
    4. main, develop 브랜치 rule 생성
    5. local에 클론해서 해당 폴더에 xcode 프로젝트 생성

---
+++ Caladium/Caladium/CaladiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaladiumApp.swift
+//  Caladium
+//
+//  Created by yoomin on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CaladiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
