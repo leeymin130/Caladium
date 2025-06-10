858b0887eed6bfccc273b4ae77afbb1597092b0f
yoomin <leeymin130@gmail.com>
2025-06-10 21:26:04 +0900

    chore/#10-Category Enum 정의
    정원 정글 사막 옥상

--- Caladium/Caladium/Models/Category.swift
+++ Caladium/Caladium/Models/Category.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum Category: String, CaseIterable {
+    case garden = "garden"
+    case jungle = "jungle"
+    case desert = "desert"
+    case rooftop = "rooftop"
+    
+    var displayName: String {
+        switch self {
+        case .garden:
+            return "정원"
+        case .jungle:
+            return "정글"
+        case .desert:
+            return "사막"
+        case .rooftop:
+            return "옥상"
+        }
+    }
+}
