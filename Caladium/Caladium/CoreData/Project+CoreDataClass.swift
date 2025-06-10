ab6db1d600d8507e13af401010837ebba8c381be
yoomin <leeymin130@gmail.com>
2025-06-10 21:53:50 +0900

    chore/#10-NSManagedObject 서브클래스 커스텀
    - Project+CoreDataClass 파일 수정
    - Photo+CoreDataClass 파일 수정

--- Caladium/Caladium/CoreData/Project+CoreDataClass.swift
+++ Caladium/Caladium/CoreData/Project+CoreDataClass.swift
@@ -11,5 +11,35 @@ import CoreData
 
 @objc(Project)
 public class Project: NSManagedObject {
-
+    
+    // Category enum 연동
+    var categoryEnum: Category {
+        get {
+            return Category(rawValue: category ?? "") ?? .garden
+        }
+        set {
+            category = newValue.rawValue
+        }
+    }
+    
+    // 편의 초기화
+    convenience init(context: NSManagedObjectContext, name: String, category: Category) {
+        self.init(context: context)
+        self.id = UUID()
+        self.name = name
+        self.category = category.rawValue
+        self.createdDate = Date()
+        self.updatedDate = Date()
+    }
+    
+    // 업데이트 메서드
+    func updateTimestamp() {
+        self.updatedDate = Date()
+    }
+    
+    // 사진 개수
+    var photoCount: Int {
+        return photos?.count ?? 0
+    }
+    
 }
