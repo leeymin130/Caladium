8aa18fd2ae1dad87678856b2fb05e12427a6f7fa
yoomin <leeymin130@gmail.com>
2025-07-14 18:54:11 +0900

    feat/#27-CameraView UI 수정

--- Caladium/Caladium/Views/Screen/CameraView.swift
+++ Caladium/Caladium/Views/Screen/CameraView.swift
@@ -19,51 +19,62 @@ struct CameraView: View {
     }
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack(spacing: 20) {
-                Text("📷 카메라")
-                    .font(.largeTitle)
-                    .bold()
+        VStack(spacing: 0) {
+            // 상단 툴바 영역
+            HStack {
+                Button {
+                    vm.cancel()
+                } label: {
+                    Image("arrow-back-w")
+                }
                 
-                Text(contextDescription)
-                    .foregroundColor(.secondary)
-                
-                // 카메라 프리뷰 영역
-                CameraViewController(cameraService: vm.cameraService)
-                    .aspectRatio(4/3, contentMode: .fit)
-            }
-            
-            Spacer()
-            
-            
-            HStack(spacing: 55){
-
                 Spacer()
+            }
+            .padding(24)
+            .background(Color.gray900)
+            
+            // 카메라 프리뷰 영역
+            CameraViewController(cameraService: vm.cameraService)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.gray900)
+            
+            
+            // 하단 조작부
+            HStack(spacing: 0){
+                Spacer()
+                
+                Button {
+                    // 가짜 버튼
+                } label: {
+                    Rectangle()
+                        .fill(Color.clear)
+                        .frame(width: 75, height: 78)
+                    
+                }
                 
                 // 촬영 버튼
                 Button {
                     vm.capturePhoto()
                     // 임시 이미지로 다음 단계로
-//                    let tempImage = UIImage(systemName: "photo") ?? UIImage()
+                    //                    let tempImage = UIImage(systemName: "photo") ?? UIImage()
                     
                 } label: {
                     Circle()
-                        .fill(Color.white)
-                        .frame(width: 80, height: 80)
+                        .fill(Color.green500)
+                        .frame(width: 78, height: 78)
                         .overlay {
                             Circle()
-                                .stroke(Color.black, lineWidth: 3)
-                                .frame(width: 70, height: 70)
+                                .fill(Color.gray0)
+                                .frame(width: 64, height: 64)
                         }
+                        .padding(.horizontal, 50)
                 }
                 
                 // 오버레이 토글
                 VStack(alignment: .center, spacing: 9) {
-                    Button(action: {
+                    Button{
                         vm.switchOverlay()
-                    }) {
+                    } label: {
                         ZStack {
                             RoundedRectangle(cornerRadius: 100)
                                 .fill(vm.isOverlayOn ? Color.green400 : Color.gray500)
@@ -80,37 +91,22 @@ struct CameraView: View {
                     Text("최근 사진 필터")
                         .font(.caption)
                 }
-                .padding(.horizontal, 16)
+                
+                Spacer()
                 
             }
-            .padding(.bottom, 50)
+            .padding(.top, 54)
+            .padding(.bottom, 20) // 하단 패딩값 확인
+            .background(Color.gray900)
             
         }
-        .background(Color.black)
-        .foregroundColor(.white)
+        .foregroundColor(.gray0)
         .navigationBarHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .bottomBar) {
-                Button("취소") {
-                    vm.cancel()
-                }
-                .font(.headline)
-                .foregroundColor(.white)
-            }
-        }
         .onAppear {
             print("🔥 onAppear 호출됨")
             vm.cameraService.requestCameraPermission()
         }
-    }
-    
-    private var contextDescription: String {
-        switch context {
-        case .newProject:
-            return "새로운 식물의 첫 번째 사진을 찍어보세요"
-        case .existingProject(let project):
-            return "\(project.categoryEnum.displayName) 프로젝트에 사진을 추가합니다"
-        }
+        
     }
 }
 
